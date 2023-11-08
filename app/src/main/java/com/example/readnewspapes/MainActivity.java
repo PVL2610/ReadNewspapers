2591220f80c020164d9a5fc712b8855503d8382e
pvl <phanvanlinh261003@gmail.com>
2023-11-08 22:52:42 +0700

    update toolbar

--- app/src/main/java/com/example/readnewspapes/MainActivity.java
+++ app/src/main/java/com/example/readnewspapes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.readnewspapes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -37,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         lvTieuDe = (ListView) findViewById(R.id.listViewTitle);
         arrayLink = new ArrayList<String>();
